57d60990f56b62b5c54c90b056b15623640d7dba
Jeremy Song <jeremy.song@me.com>
2014-05-07 20:49:47 -0400

    Clean imports

--- src/GameUI.java
+++ src/GameUI.java
@@ -1,8 +1,5 @@
-import javafx.scene.text.*;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
